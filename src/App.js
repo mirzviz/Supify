e6cdfba8c8e45a05f453c1bad6bb4c5aa80baee8
Zvi Mirimsky <mirzviz@gmail.com>
2019-11-03 20:07:43 +0200

    dfs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
+useEffect(() => {
+  const {} = getUrlParams();
+});
+
+const getUrlParams = () => {
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  var accessToken = url.searchParams.get("access_token");
+  console.log(accessToken);
+  var refreshToken = url.searchParams.get("refresh_token");
+  console.log(refreshToken);
+
+};
 function App() {
-  return (
-    <div className="App">
-      Hello Supe
-    </div>
-  );
+  return <div className="App">Hello Supe</div>;
 }
 
 export default App;
